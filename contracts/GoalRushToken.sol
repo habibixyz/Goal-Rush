97ac7988cf82301e22fa7ff4547167a10b100c4e
habibxyz <tanizcoldz@gmail.com>
2026-06-13 00:25:17 +0530

    refactor: add zero-address safety checks to GoalRushToken

--- contracts/GoalRushToken.sol
+++ contracts/GoalRushToken.sol
@@ -26,6 +26,7 @@ contract GoalRushToken {
     }
 
     function transfer(address _to, uint256 _value) public returns (bool success) {
+        require(_to != address(0), "ERC20: transfer to the zero address");
         require(balanceOf[msg.sender] >= _value, "Insufficient balance");
         balanceOf[msg.sender] -= _value;
         balanceOf[_to] += _value;
@@ -40,6 +41,7 @@ contract GoalRushToken {
     }
 
     function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require(_to != address(0), "ERC20: transfer to the zero address");
         require(balanceOf[_from] >= _value, "Insufficient balance");
         require(allowance[_from][msg.sender] >= _value, "Insufficient allowance");
         balanceOf[_from] -= _value;
